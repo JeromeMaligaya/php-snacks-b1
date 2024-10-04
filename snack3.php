1b95a7961b0cf293b1bca0894b1763f167835e9a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-04 12:48:55 +0200

    to fix structure

---
+++ snack3.php
@@ -0,0 +1,80 @@
+<!-- utilizzare questo array: https://pastebin.com/CkX3680A. 
+Stampiamo il nostro array mettendo gli insegnanti in un rettangolo grigio e i PM in un rettangolo verde. -->
+
+<?php
+ 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+ 
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Melvin Jerome Luna Maligaya">
+    <title>???????????</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <main>
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-12">
+                    <h1>snack 3</h1>
+                    <ul>
+                        <?php foreach($db as $item){?>
+                        <li>
+                            <h2><?php echo $item?></h2>
+                            <ul>
+                                <?php foreach($item as $singleItem){?>
+                                <li>
+                                    <p><?php echo $singleItem['name']?></p>
+                                    <p><?php echo $singleItem['lastname']?></p>
+
+                                </li>
+                                <?php }?>
+                            </ul>
+                        </li>
+                        <?php }?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- js bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
